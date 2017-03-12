58b0bc94811226cdc07d88a66181a096915ad2e6
James Miller <jrmiller84@gmail.com>
2017-03-12 11:11:48 -0400

    Adding config and recent code changes.

---
+++ main.js
@@ -0,0 +1,34 @@
+var gpio = require("pi-gpio"),
+    config = require(".config");
+
+if(config == null) { return; }
+
+if(config.pins == null || config.pins.legnth <= 0) {
+  return;
+}
+
+var openPin = function(pin) {
+
+  if(pin == null) return;
+
+  gpio.open(pin.pin, null, function(err) {
+
+    if(err) { console.log("Error opening pin: " + err); }
+
+    console.log(pin);
+    
+  });
+
+};
+
+var configurePins = function(c) {
+  for(var i = 0; i < c.pins.length; i++) {
+
+    var pin = c.pins[i];
+
+    openPin(pin);
+  }
+
+};
+
+configurePins(config);
