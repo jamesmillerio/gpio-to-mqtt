2b147ecea14219aafa27e38e2d021a9eeda0fa8e
James Miller <jrmiller84@gmail.com>
2017-03-12 12:54:57 -0400

    Converting pins to promises

--- main.js
+++ main.js
@@ -1,36 +1,29 @@
 var gpio = require("pi-gpio"),
+    promise = require("promise"),
     config = require("./config.json");
 
-if(config == null) { return; }
+if(config == null || config.pins == null) { return; }
 
-if(config.pins == null || config.pins.legnth <= 0) {
-  return;
+var promises = [];
+
+//Convert pins to promises.
+for(var i = 0; i < config.pins.length; i++) {
+
+  var pin = config.pins[i];
+
+  promises.push(new Promise(function(resolve, reject) {
+    gpio.open(pin.pin, null, function(err) {
+
+      if(err) { console.log("Error opening pin: " + err); }
+
+      resolve(pin)
+
+    });
+  }));
+
 }
 
-var openPin = function(pin) {
-
-  if(pin == null) return;
-
-  gpio.open(pin.pin, null, function(err) {
-
-    if(err) { console.log("Error opening pin: " + err); }
-
-    console.log(pin);
-
-  });
-
-};
-
-var configurePins = function(c) {
-  return new Promise();
-  /*for(var i = 0; i < c.pins.length; i++) {
-
-    var pin = c.pins[i];
-
-    openPin(pin);
-  }*/
-
-
-};
-
-configurePins(config);
+Promise.race(promises).then(function(pin) {
+  console.log("PIN!");
+  console.log(pin);
+})
